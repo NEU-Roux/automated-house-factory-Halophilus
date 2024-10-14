f62cd9f3ed7cc11ed7956dadd95cd7770b2da83a
Ben Henshaw <33325217+Halophilus@users.noreply.github.com>
2024-10-14 18:12:23 -0400

    Implement Block functionality

--- untitled/src/Block.java
+++ untitled/src/Block.java
@@ -5,4 +5,24 @@
  * Notes: Complex factory simulation
  */
 abstract public class Block {
+    private ResourceType resourceType;
+    private double unitWeight;
+
+    // Standard constructor
+    public Block(ResourceType resourceType, int unitWeight) {
+        setResourceType(resourceType);
+        setUnitWeight(unitWeight);
+    }
+
+    // House constructor
+    // Takes two
+    public Block(){}
+
+    // resourceType getters and setters
+    public ResourceType getResourceType() {return resourceType;}
+    public void setResourceType(ResourceType resourceType) {this.resourceType = resourceType;}
+
+    // unitWeight getters and setters
+    public double getUnitWeight() {return unitWeight;}
+    public void setUnitWeight(double unitWeight) {this.unitWeight = unitWeight;}
 }
