8bfc74d59d8e84bc2bfd72a71c78c886decf5058
Ben Henshaw <33325217+Halophilus@users.noreply.github.com>
2024-10-16 21:08:46 -0400

    Revise ResourceType enum

--- untitled/src/ResourceType.java
+++ untitled/src/ResourceType.java
@@ -5,16 +5,23 @@
  * Notes: Complex factory simulation
  */
 public enum ResourceType {
+    // Repository of ResourceType data
+    // Public static final ResourceType STONE = new ResourceType(5.5);
     STONE(5.5),
     WOOD(2.5),
+
+    // Crafted ResourceTypes
     HOUSE(STONE.weight*Const.WOOD_IN_HOUSE
                + WOOD.weight*Const.STONE_IN_HOUSE);
 
     private final double weight;
+
     ResourceType(double weight) {
         this.weight = weight;
     }
+
     public double getWeight() {
         return this.weight;
     }
 }
+
